01a619c21ec084f374ba78f61bcf5b380436b5f9
Maryam-710 <shm71000@gmail.com>
2024-11-23 13:35:27 +0500

    My name is Maryam Sheikh.
    MIT SOFTWARE SKILLS INSTITUTE.

---
+++ maryam.js
@@ -0,0 +1,22 @@
+// first we grab the element
+let a = document.getElementById("one");
+// console.log(a);
+// getting content of p element that is stored in "a" and print it to console.
+// console.log(a.innerText);
+// console.log(a.innerHTML);
+// console.log(a.textContent);
+
+
+a.style.color="yellow";
+a.style.backgroundColor="red";
+a.style.fontSize="40px";
+a.style.border="4px double yellow";
+a.style.textAlign="center";
+// a.style.padding="2px 50px 2px 50px";
+
+
+
+// how to change styling 
+
+
+a.style.textAlign="right";
